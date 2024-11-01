9f914ffd63c0fb09727ded4409610a4e1cf19860
Kirti11 <kirtijhala1110@gmail.com>
2024-11-01 15:12:12 +0530

    Create NumberPatternRhombus09.java

---
+++ NumberPatternRhombus09.java
@@ -0,0 +1,38 @@
+/*  Enter the number of rows: 5
+        1
+       123
+      12345
+     1234567
+    123456789
+     1234567
+      12345
+       123
+        1               */
+
+import java.util.Scanner;
+public class NumberPatternRhombus09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= n - 1; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * (n - i) - 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
