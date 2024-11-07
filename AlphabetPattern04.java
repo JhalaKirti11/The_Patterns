18bab65efe2e208d9c310ec671b41ae14e3b579d
Kirti11 <kirtijhala1110@gmail.com>
2024-11-07 23:11:27 +0530

    Create AlphabetPattern04.java

---
+++ AlphabetPattern04.java
@@ -0,0 +1,28 @@
+/*    E
+      DE
+      CDE
+      BCDE
+      ABCDE      */
+
+import java.util.Scanner;
+public class AlphabetPattern04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+       
+		int n = sc.nextInt();
+		int i = 1;
+		while(i <= n) {
+			int j = 1;
+			char character = (char)('A' + n - i);
+			while(j <= i) {
+				System.out.print(character);
+				character++;
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+        sc.close();
+    }
+}
