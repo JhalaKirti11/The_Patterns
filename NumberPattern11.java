473649652b0a5d475fec4021e07bd254902d28eb
Kirti11 <kirtijhala1110@gmail.com>
2024-12-22 23:08:59 +0530

    Update NumberPattern11.java

--- NumberPattern11.java
+++ NumberPattern11.java
@@ -4,18 +4,19 @@
       12221
       11111      */
 
+import java.util.Scanner;
 public class NumberPattern02 {
     public static void main(String[] args) {
-        int n = 5;
+      Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         int k = n / 2 + 1;
-
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if ((i <= k && j <= i) || (i >= k && j <= n - i + 1)) {
                     System.out.print(j);
                 } else if (i <= k && j <= n - i + 1) {
                     System.out.print(i);
-                } else if (i >= k && j <= 2 * i - n) {
+                } else if (i >= k && j <= 2 * i - n + 1) {
                     System.out.print(n - i + 1);
                 } else {
                     System.out.print(n - j + 1);
@@ -23,5 +24,6 @@ public class NumberPattern02 {
             }
             System.out.println();
         }
+          sc.close();
     }
 }
