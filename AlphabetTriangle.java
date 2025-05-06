e90bcd547e081aeef9d63a988702ff1cbedd10ba
Kirti11 <kirtijhala1110@gmail.com>
2025-05-06 22:09:53 +0530

    Create AlphabetTriangle.java

---
+++ AlphabetTriangle.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+/*
+E
+DE
+CDE
+BCDE
+ABCDE
+*/
+
+public class AlphabetTriangle {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows: ");
+
+		int n = sc.nextInt();
+		int i = 1;
+		while (i <= n) {
+			int j = 1;
+			char character = (char) ('A' + n - i);
+			while (j <= i) {
+				System.out.print(character);
+				character++;
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+		sc.close();
+	}
+}
